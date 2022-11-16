14105c2be51b2de4608059629e7845487d88e12d
Zhang Yiting <d1325981@ncsdevnet.com>
2022-11-16 16:49:35 +0800

    failtest

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,12 +31,15 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-		String o1 = null;  
+/*		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
 		assertTrue (stack.isEmpty());
         stack.push (s1);
         stack.push (s2);
-        assertFalse (stack.isEmpty());
+        assertFalse (stack.isEmpty());*/
+		MyStringStack stack = new MyStringStack();
+		stack.push (s1);
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
